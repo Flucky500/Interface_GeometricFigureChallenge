524dcbe668d4aa57a364ce91944f13382e4d0963
Lukasz Pasko <flucky500@gmail.com>
2022-03-15 22:51:24 +0100

    Initial commit

---
+++ src/main/java/pl/pasko/GeometricFigure.java
@@ -0,0 +1,9 @@
+package pl.pasko;
+
+public interface GeometricFigure {
+
+    double area ();
+    double perimeter();
+
+
+}
---
+++ src/main/java/pl/pasko/Main.java
@@ -0,0 +1,10 @@
+package pl.pasko;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10,12);
+        System.out.println(rectangle.area());
+        System.out.println(rectangle.perimeter());
+    }
+}
